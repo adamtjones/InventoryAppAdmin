080f7434217c9fc57e15bfc18e125fa6a639d990
Adam Jones <adamjns802@gmail.com>
2016-07-14 15:00:13 -0400

    updated with grading notes

--- js/main.js
+++ js/main.js
@@ -3,36 +3,116 @@
     angular
         .module('inventory', [])
         .controller('HeaderController', function Header() {
-            this.data = [
-			  { "id": 2957, "name": "widget", "price": 32, "quantity": 203, "color": "red", "discount": 31 },
-			  { "id": 89274, "name": "golf club", "price": 98, "quantity": 10, "color": "black", "discount": 0 },
-			  { "id": 64, "name": "iPhone", "price": 499, "quantity": 2, "color": "white", "discount": 0 },
-			  { "id": 87363, "name": "bonzai tree", "price": 76, "quantity": 2, "color": "green", "discount": 0 },
-			  { "id": 1736, "name": "towel", "price": 55, "quantity": 30, "color": "brown", "discount": 10 },
-			  { "id": 4836, "name": "dog bed", "price": 99, "quantity": 10, "color": "beige", "discount": 50 },
-			  { "id": 829, "name": "waste basket", "price": 15, "quantity": 40, "color": "silver", "discount": 0 },
-			  { "id": 46, "name": "guitar", "price": 899, "quantity": 3, "color": "blue", "discount": 150 },
-			  { "id": 17456, "name": "matcha tea", "price": 42, "quantity": 4, "color": "green", "discount": 11 },
-			  { "id": 3292, "name": "enlightenment", "price": 99999, "quantity": 1, "color": "red", "discount": 0 },
-			  { "id": 533, "name": "eggs", "price": 5, "quantity": 12, "color": "brown", "discount": 1 },
-			  { "id": 683, "name": "pillow", "price": 27, "quantity": 10, "color": "black", "discount": 12 }
-			];
+            var vm = this;
 
-	var randomNumber = Math.floor(Math.random()*20001);
-	console.log(randomNumber);
+            vm.data = [{
+                "id": 2957,
+                "name": "widget",
+                "price": 32,
+                "quantity": 203,
+                "color": "red",
+                "discount": 31
+            }, {
+                "id": 89274,
+                "name": "golf club",
+                "price": 98,
+                "quantity": 10,
+                "color": "black",
+                "discount": 0
+            }, {
+                "id": 64,
+                "name": "iPhone",
+                "price": 499,
+                "quantity": 2,
+                "color": "white",
+                "discount": 0
+            }, {
+                "id": 87363,
+                "name": "bonzai tree",
+                "price": 76,
+                "quantity": 2,
+                "color": "green",
+                "discount": 0
+            }, {
+                "id": 1736,
+                "name": "towel",
+                "price": 55,
+                "quantity": 30,
+                "color": "brown",
+                "discount": 10
+            }, {
+                "id": 4836,
+                "name": "dog bed",
+                "price": 99,
+                "quantity": 10,
+                "color": "beige",
+                "discount": 50
+            }, {
+                "id": 829,
+                "name": "waste basket",
+                "price": 15,
+                "quantity": 40,
+                "color": "silver",
+                "discount": 0
+            }, {
+                "id": 46,
+                "name": "guitar",
+                "price": 899,
+                "quantity": 3,
+                "color": "blue",
+                "discount": 150
+            }, {
+                "id": 17456,
+                "name": "matcha tea",
+                "price": 42,
+                "quantity": 4,
+                "color": "green",
+                "discount": 11
+            }, {
+                "id": 3292,
+                "name": "enlightenment",
+                "price": 99999,
+                "quantity": 1,
+                "color": "red",
+                "discount": 0
+            }, {
+                "id": 533,
+                "name": "eggs",
+                "price": 5,
+                "quantity": 12,
+                "color": "brown",
+                "discount": 1
+            }, {
+                "id": 683,
+                "name": "pillow",
+                "price": 27,
+                "quantity": 10,
+                "color": "black",
+                "discount": 12
+            }];
 
-		this.submit = function (isValid) {
-		var newObj = {"id": randomNumber, "name": this.form.name, "price": this.form.price, "quantity": this.form.quantity, "color": this.form.color, "discount": this.form.discount };
-		
-		if (isValid) {
-			this.data.push(newObj);
-			this.form = [];
-	}
-	
-	};
+            var randomNumber = Math.floor(Math.random() * 20001);
+            console.log(randomNumber);
 
-        
-    });
+            vm.submit = function(isValid) {
+                var newObj = {
+                    "id": randomNumber,
+                    "name": vm.form.name,
+                    "price": vm.form.price,
+                    "quantity": vm.form.quantity,
+                    "color": vm.form.color,
+                    "discount": vm.form.discount
+                };
+
+                if (isValid) {
+                    vm.data.push(newObj);
+                    vm.form = [];
+                }
+
+            };
+
+
+        });
 })();
 
 
